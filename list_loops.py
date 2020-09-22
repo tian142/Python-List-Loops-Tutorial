e333b6db51a3ca12c8857f074de9b5c9f08df085
Tian Qiu <tian.qiu142@gmail.com>
2020-09-22 15:56:32 -0700

    completed assignment

--- list_loops.py
+++ list_loops.py
@@ -1,18 +1,29 @@
 songs = ["ROCKSTAR", "Do It", "For The Night"]
-print(songs[1])
-print(songs[0:2])
 
-# update the first element
-songs[0] = "Dynamite"
-# print the list
+# task 1: print first and last element of song list
+print(songs[0], ", ", songs[2])
+
+# task 2: print out "Do It"and "For The Night" using a list slice on the songs list
+print(songs[1:3])
+
+# task 3:  update the last element in the songs list with a new song of your choice
+songs[2] = "僕は一寸"
 print(songs)
 
-# adds an element to the end of the list
-songs.append("Mood")
-# adds an element to the beginning of a list
-# songs.prepend("Roses")
-# adds a list to end of a list
-songs.extend(["Laugh Now Cry Later", "Blinding Lights"])
-# adds element at specific index followed by item
-songs.insert(0, "Life Is Good")
+# Task 4: add three songs of your choice to your songs list
+songs.extend(["사랑하기 때문에", "Gymnopédie No. 1", "J'en Ai Marre"])
 print(songs)
+
+# Task 5: delete one of the elements in your songs list in your list_loops.py file
+songs.pop(5)
+print(songs)
+
+
+animals = ["Cat", "Dog", "Bird"]
+animals.append("Axolot")
+print(animals[2])
+animals.pop(0)
+print(animals)
+
+for animal in animals:
+    print(animal)
